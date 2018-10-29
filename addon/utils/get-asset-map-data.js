ab3efc4b17e290567c1e8b47f070aeb35c6b3cfd
Ruslan Zavacky <ruslan.zavacky@crowdstrike.com>
2018-10-29 16:27:20 +0000

    Fix missing DOM APIs from fastboot mode

--- addon/utils/get-asset-map-data.js
+++ addon/utils/get-asset-map-data.js
@@ -1,5 +1,12 @@
 export default function getAssetMapData() {
+  const isDocumentUndefined = typeof document === 'undefined';
+
+  if (isDocumentUndefined) {
+    return;
+  }
+
   const assetMapString = document.querySelector("meta[name='ember-cli-ifa:assetMap']").content;
+
   if (!assetMapString) {
     return;
   }
