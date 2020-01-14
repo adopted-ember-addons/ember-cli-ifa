1bb102e2863fdf7083eb4e955ca54ac9daa5b204
= <m.bloemendal@gynzy.com>
2020-01-14 13:32:19 +0100

    add ember-fetch

--- addon/initializers/asset-map.js
+++ addon/initializers/asset-map.js
@@ -2,6 +2,7 @@ import RSVP from 'rsvp';
 import AssetMap from '../services/asset-map';
 import { typeOf as getTypeOf } from '@ember/utils';
 import getAssetMapData from 'ember-cli-ifa/utils/get-asset-map-data';
+import fetch from 'ember-fetch';
 
 export function initialize(app) {
   let assetMapFile = getAssetMapData();
@@ -15,40 +16,40 @@ export function initialize(app) {
     AssetMap.reopen({
       map: assetMapFile.assets,
       prepend: assetMapFile.prepend,
-      enabled: true
+      enabled: true,
     });
     app.register('service:asset-map', AssetMap);
   } else {
-    
-    if (typeof app.deferReadiness === "function") {
+    if (typeof app.deferReadiness === 'function') {
       app.deferReadiness();
     }
 
     const promise = fetch(assetMapFile).then(response => response.json());
 
-    promise.then((map = {}) => {
-      AssetMap.reopen({
-        map: map.assets,
-        prepend: map.prepend,
-        enabled: true
+    promise
+      .then((map = {}) => {
+        AssetMap.reopen({
+          map: map.assets,
+          prepend: map.prepend,
+          enabled: true,
+        });
+      })
+      .then(() => {
+        app.register('service:asset-map', AssetMap);
+        if (typeof app.advanceReadiness === 'function') {
+          app.advanceReadiness();
+        }
+      })
+      .catch(err => {
+        console.error('Failed to register service:asset-map', err);
+        if (typeof app.advanceReadiness === 'function') {
+          app.advanceReadiness();
+        }
       });
-    })
-    .then(() => {
-      app.register('service:asset-map', AssetMap);
-      if (typeof app.advanceReadiness === "function") {
-        app.advanceReadiness();
-      }
-    })
-    .catch((err) => {
-      console.error('Failed to register service:asset-map', err);
-      if (typeof app.advanceReadiness === "function") {
-        app.advanceReadiness();
-      }
-    });
   }
 }
 
 export default {
   name: 'asset-map',
-  initialize
+  initialize,
 };
